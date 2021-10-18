ae83190ce5653a8caa73dadbc7df0adb5b0b18c4
Eugen Liplianin <91380950+EugenLipa@users.noreply.github.com>
2021-10-18 13:17:47 +0300

    Add files via upload

---
+++ home.work3.js
@@ -0,0 +1,43 @@
+//задача 1//
+/*
+let x=10, y=5;
+ x=(Math.sqrt(x,2));
+ y=(Math.pow(y,2));
+ x=(x+1);
+ y=(y+x);
+console.log(y);
+*/
+
+//задача 2//
+/*
+let myVar, myResult;
+myVar = 15;
+
+if (myVar<0) { 
+        myResult=-Math.pow(myVar,2)
+    } 
+if (myVar>0) {
+        myResult=Math.pow(myVar,2)
+    }
+console.log (myResult); 
+*/
+
+//задача 3//
+/*
+let a = 0, b = 1; 
+b = a+b;
+a = b+1;
+for ( a= 0 ; a<5 ; a++ )
+console.log (a) 
+*/
+
+
+/*
+let a = 0, b = 1; 
+b = a+b;
+a = b+1;
+    while (a<5){
+        a++
+    }
+console.log (a) 
+*/
